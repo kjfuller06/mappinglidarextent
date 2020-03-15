ee74b979e9fd35cae98d04334eda9ed7ccb5aed0
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-03-15 22:22:19 +1100

    Trying to update with colours by year. WIP

--- MappingLiDARextent_MJShapefiles.R
+++ MappingLiDARextent_MJShapefiles.R
@@ -5,25 +5,40 @@ library(mapview)
 LiDAR1 <- st_read(
   "zone54_s3_tiles.shp")
 st_crs(LiDAR1)<-28354
-
 LiDAR2 <- st_read(
   "zone55_s3_tiles.shp")
 st_crs(LiDAR2)<-28355
-
 LiDAR3 <- st_read(
   "zone56_s3_tiles.shp")
 st_crs(LiDAR3)<-28356
 
-a<-st_union(LiDAR1,by_feature=FALSE)
-a<-st_transform(a,crs=28356)
-b<-st_union(LiDAR2,by_feature=FALSE)
-b<-st_transform(b,crs=28356)
-c<-st_union(LiDAR3,by_feature=FALSE)
+a<-st_transform(LiDAR1,crs=28356)
+b<-st_transform(LiDAR2,crs=28356)
 
-d<-st_union(a,b,by_feature=FALSE)
-d<-st_union(d,c,by_feature=FALSE)
+LiDAR1<-LiDAR1 %>% 
+  group_by(year) %>%
+  summarise(geometry = sf::st_union(geometry)) %>%
+  ungroup() %>% 
+  st_cast("MULTIPOLYGON")
+LiDAR2<-LiDAR2 %>% 
+  group_by(year) %>%
+  summarise(geometry = sf::st_union(geometry)) %>%
+  ungroup()%>% 
+  st_cast("MULTIPOLYGON")
+LiDAR3<-LiDAR3 %>% 
+  group_by(year) %>%
+  summarise(geometry = sf::st_union(geometry)) %>%
+  ungroup()%>% 
+  st_cast("MULTIPOLYGON")
 
-expo<-mapview(d, alpha=0)
+# a<-st_union(LiDAR1,by_feature=TRUE)
+# d<-st_union(a,b,by_feature=FALSE)
+# d<-st_union(d,c,by_feature=FALSE)
+
+expo<-mapview(list(LiDAR1,LiDAR2,LiDAR3),col.regions= rainbow, at = c(2009:2018), alpha=0)
+expo
+
+
 
 mapshot(expo,file="lidarplots.png")
 
