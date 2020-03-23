fa77cec1844d97048f126c1740d9b40688d23fe9
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-03-23 21:05:04 +1100

    Gave up on fixing the map. I just edited it in Paint

--- MappingLiDARextent_MJShapefiles.R
+++ MappingLiDARextent_MJShapefiles.R
@@ -1,5 +1,6 @@
 library(sf)
 library(mapview)
+library(tidyverse)
 
 #coordinate reference systems grabbed from Spatial Services website, it's also the naming system for the "zones"- using just the last two digits of the CRS
 LiDAR1 <- st_read(
@@ -18,29 +19,29 @@ b<-st_transform(LiDAR2,crs=28356)
 LiDAR1<-LiDAR1 %>% 
   group_by(year) %>%
   summarise(geometry = sf::st_union(geometry)) %>%
-  ungroup() %>% 
-  st_cast("MULTIPOLYGON")
+  ungroup() 
+  # st_cast("MULTIPOLYGON")
 LiDAR2<-LiDAR2 %>% 
   group_by(year) %>%
   summarise(geometry = sf::st_union(geometry)) %>%
-  ungroup()%>% 
-  st_cast("MULTIPOLYGON")
+  ungroup()
+  # st_cast("MULTIPOLYGON")
 LiDAR3<-LiDAR3 %>% 
   group_by(year) %>%
   summarise(geometry = sf::st_union(geometry)) %>%
-  ungroup()%>% 
-  st_cast("MULTIPOLYGON")
+  ungroup()
+  # st_cast("MULTIPOLYGON")
 
 # a<-st_union(LiDAR1,by_feature=TRUE)
 # d<-st_union(a,b,by_feature=FALSE)
 # d<-st_union(d,c,by_feature=FALSE)
 
-expo<-mapview(list(LiDAR1,LiDAR2,LiDAR3),col.regions= rainbow, at = c(2009:2018), alpha=0)
+expo<-mapview(list(LiDAR1,LiDAR2,LiDAR3),col.regions= rainbow, at = c(2009:2018), alpha=0, legend =FALSE)
 expo
 
 
 
-mapshot(expo,file="lidarplots.png")
+mapshot(expo,file="lidarplots2.png")
 
 e<-as_Spatial(d)
 gArea(e)/1000000
